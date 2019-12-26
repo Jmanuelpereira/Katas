e8c64853cfeb37b1ede6b44491622a481c488dcc
Jose Pereira <jmanuelpereira2@gmail.com>
2019-12-25 22:06:43 -0500

    addin katas

---
+++ noVowels.js
@@ -0,0 +1,16 @@
+function disemvowel(str) {
+
+    const arrLetters = str
+    let noVowels = [];
+    
+        for(i = 0; i <= arrLetters.length; i++){
+    
+          noVowels = arrLetters.replace(/[aeiouAEIOU]/g,'')
+    
+       
+    
+        }//ending for i
+    
+        return noVowels
+    
+    }
